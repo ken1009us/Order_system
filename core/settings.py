124fccb2fec55d5e9fa5c94e2a73072e861e8493
Ken Wu <kenwu1009us@gmail.com>
2021-02-05 14:57:17 +0800

    Modify settings code

--- core/settings.py
+++ core/settings.py
@@ -33,15 +33,22 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 # Application definition
 
 # Add apps (orders, users)
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user'
 ]
+
+THIRD_PARTY_APPS = [
+    'django_extensions',
+]
+
+LOCAL_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
