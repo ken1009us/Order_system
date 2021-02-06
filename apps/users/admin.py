c1437472a968acfb8d5d27efdbcd5778ed9c2bf2
Ken Wu <kenwu1009us@gmail.com>
2021-02-06 20:13:08 +0800

    modify users admin.py

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -7,10 +7,16 @@ from .models import User
 @admin.register(User)
 class UserAdmin(AuthUserAdmin):
     list_display = (
-        'username',
-        'sex',
-        'email',
-        'first_name',
-        'last_name',
-        'is_staff',
+        'username', 'sex', 'email', 'first_name', 'last_name', 'is_staff',
     )
+
+    fieldsets = [
+        (None, {'fields': ['email', 'password']}),
+        ('Personal info', {'fields': ['first_name', 'last_name', 'sex']}),
+        ('Permissions', {'fields': ['is_active',
+                                    'is_staff',
+                                    'is_superuser',
+                                    'groups',
+                                    'user_permissions']}),
+        ('Important dates', {'fields': ['last_login', 'date_joined']}),
+    ]
